daef1f6f30d52945c112c4c5bf5dc32ff7f00e7f
nohaahed <nohaahed209@gmail.com>
2023-09-13 01:11:34 +0200

    Create AuctionListing model

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,25 @@ from django.db import models
 
 
 class User(AbstractUser):
+    pass 
+
+class Category(models.Model):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    starting_price = models.FloatField()
+    description = models.CharField(max_length=512)
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
+    photo = models.URLField(blank=True , default='')
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="listings")
+    watchers = models.ManyToManyField(User, blank=True, related_name="watchlist")
+       
+
+class Bid(models.Model):
+    pass
+
+class CommentsOnAuction(models.Model):
+    pass
+
