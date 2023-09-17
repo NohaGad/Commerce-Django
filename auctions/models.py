5db387bcda947f7e02ac69fdaede1156df9a4727
nohaahed <nohaahed209@gmail.com>
2023-09-18 01:21:04 +0200

    Make a category name unique

--- auctions/models.py
+++ auctions/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     pass 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique= True)
     
     def __str__(self):
         return self.name
@@ -17,7 +17,7 @@ class AuctionListing(models.Model):
     starting_price = models.FloatField()
     description = models.CharField(max_length=512)
     is_active = models.BooleanField()
-    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, blank=True)
     photo = models.URLField(blank=True , default='')
     owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="listings")
     watchers = models.ManyToManyField(User, blank=True, related_name="watchlist")
