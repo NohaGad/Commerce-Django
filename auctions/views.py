7b830c26369e3e1d0254abe089e4fa4a2ee503e4
nohaahed <nohaahed209@gmail.com>
2023-09-18 02:57:02 +0200

    Add Active listing to default route

--- auctions/views.py
+++ auctions/views.py
@@ -21,7 +21,9 @@ class NewAuctionForm(forms.Form):
     
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_auctions = AuctionListing.objects.filter(is_active = True)
+    DEFAULT_IMAGE ='data:image/svg+xml;charset=UTF-8,<svg%20width%3D"286"%20height%3D"180"%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20viewBox%3D"0%200%20286%20180"%20preserveAspectRatio%3D"none"><defs><style%20type%3D"text%2Fcss">%23holder_18aa591097b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20<%2Fstyle><%2Fdefs><g%20id%3D"holder_18aa591097b"><rect%20width%3D"286"%20height%3D"180"%20fill%3D"%23777"><%2Frect><g><text%20x%3D"107.1937484741211"%20y%3D"96.24000034332275">286x180<%2Ftext><%2Fg><%2Fg><%2Fsvg>'
+    return render(request, "auctions/index.html", {"active_auctions":active_auctions , "default_image":DEFAULT_IMAGE})
 
 
 def login_view(request):
@@ -106,5 +108,6 @@ def create_auction(request):
         )
  
 def listing(request):
-    return HttpResponse("HELLO")         
+    return HttpResponse("HELLO")  
+  
         
